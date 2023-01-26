789a118c0043aaff4e05e224674d8e995719e02e
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-26 12:37:32 +0530

    Feat: Add endpoint to display all done tasks and removed done asks

--- ToDos_API.js
+++ ToDos_API.js
@@ -37,7 +37,22 @@ app.post("/tasks", (req, res) => {
   res.status(201).send(todos);
 });
 
-
+// To display all done tasks and removed done tasks
+app.get("/tasks/done", (req, res) => {
+  console.log("in /tasks/done get");
+  let d = [];
+  todos.forEach(todo => {
+    if (todo.isCompleted) {
+      d.push({ name: todo.name });
+    }
+  });
+  console.log(done.length);
+  done.forEach(did => {
+    d.push({ name: did.name });
+  });
+  d = JSON.parse(JSON.stringify(d));
+  res.status(200).send(d);
+});
 
 app.listen(port, () => {
   console.log(`http://${hostname}:${port}`);
