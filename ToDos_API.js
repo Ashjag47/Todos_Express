e27ddc4e647d52056705d52a2d29f30e1f9430e2
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-26 12:32:15 +0530

    Feat: Add endpoint to display all tasks in todos list

---
+++ ToDos_API.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+const todos = [
+  {
+    id: "0",
+    name: "node js",
+    isCompleted: false
+  },
+  {
+    id: "1",
+    name: "Express js",
+    isCompleted: false
+  }
+];
+
+const done = [];
+
+// To display all the tasks
+app.get("/tasks", (req, res) => {
+  console.log("in /tasks");
+  res.status(200).send(todos);
+});
+
+
+app.listen(port, () => {
+  console.log(`http://${hostname}:${port}`);
+});
